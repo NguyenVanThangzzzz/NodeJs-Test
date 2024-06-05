fd18a4fa8068add12152907552baa0a6adaf1b2e
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2024-06-06 00:02:03 +0700

    update scss, bootstrap, POST server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-//const path = require('path');
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 // handlebars
 app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './src/resources/views');
 //
 app.use(morgan('combined'));
-
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 //
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
 });
 
 app.listen(port, () => {
